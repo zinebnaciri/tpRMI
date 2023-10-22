14fb85ff9b1180ad0d7f7883d5e94a1f8692e06a
zineb <126409044+catmilkomg@users.noreply.github.com>
2023-10-22 17:28:11 +0100

    first commit

---
+++ ClientG7/src/entities/Machine.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+
+public class Machine  implements Serializable{
+     private int id;
+     private String ref;
+     private String marque;
+     private double prix;
+
+    public Machine() {
+    }
+
+    public Machine(String ref, String marque, double prix) {
+        this.ref = ref;
+        this.marque = marque;
+        this.prix = prix;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    @Override
+    public String toString() {
+        return "Machine{" + "id=" + id + ", ref=" + ref + ", marque=" + marque + ", prix=" + prix + '}';
+    }
+     
+     
+    
+}
---
+++ ServeurG7/src/entities/Machine.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.NamedQuery;
+
+/**
+ *
+ * @author Lachgar
+ */
+@Entity
+@NamedQuery(name = "findAll", query = "from Machine") //HQL
+@NamedNativeQuery(name = "findAllNative", query = "select * from machine", resultClass = Machine.class) 
+public class Machine implements Serializable{
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private int id;
+     private String ref;
+     private String marque;
+     private double prix;
+
+    public Machine() {
+    }
+
+    public Machine(String ref, String marque, double prix) {
+        this.ref = ref;
+        this.marque = marque;
+        this.prix = prix;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    @Override
+    public String toString() {
+        return "Machine{" + "id=" + id + ", ref=" + ref + ", marque=" + marque + ", prix=" + prix + '}';
+    }
+     
+     
+    
+}
